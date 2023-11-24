34de24551b365aa199aaca50022c22911e9f860b
ManuelaJL <manuela_lutolf@hotmail.com>
2023-11-24 19:00:42 +0100

    Pseudocode getSustainability

--- pythonProj/JsonImporter.py
+++ pythonProj/JsonImporter.py
@@ -3,12 +3,15 @@ import os
 import json
 from tqdm import tqdm
 
-PATH = "Migros_case/products/products_en/en/"
-file_list = os.listdir(PATH)
 
-data = pd.DataFrame()
 
-for file in tqdm(file_list[:3]):            #Takes 20 mins. Don't run often!
-    data_open = open(PATH + file, encoding="utf8").read()
-    temp = pd.json_normalize(json.loads(data_open))
-    data  = data._append(temp, ignore_index = True)
+def importData():
+    PATH = "Migros_case/products/products_en/en/"
+    file_list = os.listdir(PATH)
+
+    data = pd.DataFrame()
+
+    for file in tqdm(file_list[:3]):            #Takes 20 mins. Don't run often!
+        data_open = open(PATH + file, encoding="utf8").read()
+        temp = pd.json_normalize(json.loads(data_open))
+        data  = data._append(temp, ignore_index = True)
--- pythonProj/main.py
+++ pythonProj/main.py
@@ -3,10 +3,23 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pandas as pd
+
+catalogue = pd.DataFrame  #todo: JsonImporter.importData()
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def getProductById(productnumber):
+    return catalogue.filter(id=productnumber)
+
+def getSustainability(product):
+    return { product.m_check2_packaging,
+     product.dimension_circularity.material_health,
+     product.dimension_circularity.product_circularity,
+     product.m_check_fish }
+
 
 
 # Press the green button in the gutter to run the script.
